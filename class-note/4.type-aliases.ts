b87dd28ae78a8389d5ab0f7c68aee88ef2bdb7a9
LeeJams <leejam208@gmail.com>
2022-01-25 22:13:16 +0900

    타입이란?

---
+++ class-note/4.type-aliases.ts
@@ -0,0 +1,16 @@
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+var seho: Person = {
+  name: '세호',
+  age: 30
+}
+
+// 타입 별칭과 인터페이스의 가장 큰 차이점은 타입은 확장이 불가능하다.
